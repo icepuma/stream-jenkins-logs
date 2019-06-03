aea886d6a839c8f4c7dcc987fd5c105ec5374991
Stefan Ruzitschka <stefan@ruzitschka.me>
2019-06-04 01:45:53 +0200

    [feat] enhance help and parameters

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::errors::Errors;
-use clap::{crate_authors, crate_name, crate_version, App, Arg};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, AppSettings, Arg};
 use std::io::{self, Write};
 use std::time::Duration;
 use url::Url;
@@ -23,21 +23,32 @@ fn app<'a>() -> App<'a, 'a> {
   App::new(crate_name!())
     .version(crate_version!())
     .author(crate_authors!())
+    .about(crate_description!())
+    .global_setting(AppSettings::ColoredHelp)
     .arg(
-      Arg::with_name("base-url")
-        .help("Jenkins URL: e.g. https://jenkins.example.com or https://user:password@jenkins.example.com")
-        .env("BASE")
+      Arg::with_name("job")
+        .help("Job")
+        .env("JOB")
+        .takes_value(true)
         .required(true),
     )
-    .arg(Arg::with_name("job").help("Job").env("JOB").required(true))
+    .arg(
+      Arg::with_name("jenkins-base-url")
+        .long("jenkins-base-url")
+        .help("Jenkins URL: e.g. https://jenkins.example.com or https://user:password@jenkins.example.com")
+        .env("JENKINS_BASE_URL")
+        .required(true),
+    )
     .arg(
       Arg::with_name("build")
+        .long("build")
         .help("Build")
         .env("BUILD")
         .default_value("lastBuild"),
     )
     .arg(
       Arg::with_name("poll-interval")
+        .long("poll-interval")
         .help("Poll interval: in ms")
         .env("POLL_INTERVAL")
         .default_value("1000"),
@@ -47,7 +58,7 @@ fn app<'a>() -> App<'a, 'a> {
 fn main() -> Result<(), Errors> {
   let matches = app().get_matches();
 
-  let base_url = Url::parse(matches.value_of("base-url").unwrap())?;
+  let jenkins_base_url = Url::parse(matches.value_of("jenkins-base-url").unwrap())?;
   let job = matches.value_of("job").unwrap();
   let build_path = job.replace("/", "/job/");
   let build = matches.value_of("build").unwrap();
@@ -61,13 +72,13 @@ fn main() -> Result<(), Errors> {
   loop {
     let url = format!(
       "{}/job/{}/{}/logText/progressiveText?start={}",
-      base_url, build_path, build, start
+      jenkins_base_url, build_path, build, start
     );
 
     let mut request_builder = client.get(&url);
 
-    if base_url.username() != "" && base_url.password().unwrap_or_default() != "" {
-      request_builder = request_builder.basic_auth(base_url.username(), base_url.password())
+    if jenkins_base_url.username() != "" && jenkins_base_url.password().unwrap_or_default() != "" {
+      request_builder = request_builder.basic_auth(jenkins_base_url.username(), jenkins_base_url.password())
     }
 
     let mut response = request_builder.send()?;
